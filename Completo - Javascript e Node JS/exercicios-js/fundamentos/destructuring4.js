5d51d6ce11dd8644635e4a703fda52e88cf42ed9
gustavo.coelho <gustavo.coelho@4ward.com.br>
2019-10-01 21:32:16 -0300

    Blank space supressed

--- Completo - Javascript e Node JS/exercicios-js/fundamentos/destructuring4.js
+++ Completo - Javascript e Node JS/exercicios-js/fundamentos/destructuring4.js
@@ -2,7 +2,6 @@ function rand( [min = 0, max = 1000] ){
     if(min > max) [min, max] = [max, min] //inverter os valores
     const valor = Math.random() * (max - min) + min
     return Math.floor(valor)
-
 }
 
 //const obj = [1001, 4]
